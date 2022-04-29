f278cd1f7ac9c3f9fdc81f6b867ae169c6c864d6
17043003 <ishizuka533725@gmail.com>
2022-04-29 20:17:42 +0900

    install socketio-client.

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-        Welcome to picture chat!
-        </h1>
+        <h1 className={styles.title}>Welcome to picture chat!</h1>
 
         <p className={styles.description}>
           Chat with drawing image and character.

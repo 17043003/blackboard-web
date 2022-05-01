e594da0032ba7417645cad84e772c3c0752ee9aa
17043003 <ishizuka533725@gmail.com>
2022-05-01 23:34:56 +0900

    connect websocket and recieve message.

---
+++ components/Chat.tsx
@@ -0,0 +1,47 @@
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import { useState, useEffect } from "react";
+
+const Chat = ({ socketUrl }: { socketUrl: string }): JSX.Element => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [receivedMsg, setReceivedMsg] = useState("");
+  const {
+    sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+  } = useWebSocket(socketUrl, {
+    share: true,
+    onOpen: () => {
+      if (readyState != ReadyState.OPEN) {
+        console.log("opened");
+        sendMessage("Connected!");
+      }
+    },
+    onMessage: (ev) => {
+      console.log(ev.data);
+      setReceivedMsg(ev.data);
+    },
+
+    shouldReconnect: (closeEvent) => {
+      return true;
+    },
+  });
+
+  useEffect(() => {
+    if (receivedMsg == "") return;
+    setMessages([...messages, receivedMsg]);
+  }, [receivedMsg]);
+  return (
+    <>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Chat;
--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import Chat from "../../components/Chat";
 
 const Room: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const baseUrl = process.env.NEXT_PUBLIC_WS_BASE_URL ?? "";
+  if (baseUrl == "") {
+    return (
+      <>
+        <h1>Websoket connect error.</h1>
+        <h2>Websocket URL is invalid.</h2>
+      </>
+    );
+  }
+
+  const socketUrl = `${baseUrl}/${id}`;
+
   return (
     <>
-      <h1>Room</h1>
+      <h1>Room:{id}</h1>
+      <Chat socketUrl={socketUrl} />
     </>
   );
 };

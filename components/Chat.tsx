3992f74c0ebb5ce3ad5a8ae72252c2b0527454bd
17043003 <ishizuka533725@gmail.com>
2022-05-04 22:41:29 +0900

    add send message through websocket.

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 const Chat = ({ socketUrl }: { socketUrl: string }): JSX.Element => {
   const [messages, setMessages] = useState<string[]>([]);
   const [receivedMsg, setReceivedMsg] = useState("");
+  const [willSendMsg, setWillSendMsg] = useState("");
   const {
     sendMessage,
     sendJsonMessage,
@@ -29,18 +30,42 @@ const Chat = ({ socketUrl }: { socketUrl: string }): JSX.Element => {
     },
   });
 
+  const handleWillSendMsg: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setWillSendMsg(e.target.value);
+  };
+
+  const handleSendMsg = () => {
+    if (willSendMsg == "") return;
+    sendMessage(willSendMsg);
+    setWillSendMsg("");
+  };
+
   useEffect(() => {
     if (receivedMsg == "") return;
     setMessages([...messages, receivedMsg]);
   }, [receivedMsg]);
   return (
-    <>
-      <ul className={"font-mono"}>
-        {messages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
-    </>
+    <div className={"flex flex-col h-full"}>
+      <div className={""}>
+        <ul className={"font-mono"}>
+          {messages.map((message, index) => (
+            <li key={index} className={"mb-4"}>
+              {message}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={"overflow-auto"}>
+        <input
+          className={"placeholder:italic placeholder:text-slate-400"}
+          type="text"
+          placeholder="Enter message"
+          onChange={handleWillSendMsg}
+          value={willSendMsg}
+        />
+        <button onClick={handleSendMsg}>Submit</button>
+      </div>
+    </div>
   );
 };
 
